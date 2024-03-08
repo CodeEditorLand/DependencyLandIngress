21098405fe0845d861a063ee6597dbe4fc51e76e
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-03-08 12:27:23 +0200

    0.0.4

--- Target/Variable/Worker.js
+++ Target/Variable/Worker.js
@@ -1,18 +1,1 @@
-var Worker_default = {
-  fetch: async (...[Request, Environment]) => await (await import("itty-router/Router")).Router().get(
-    "*",
-    async () => await Response(
-      {
-        Error: "Not Found."
-      },
-      404
-    )
-  ).handle(Request, Environment)
-};
-const { default: Redirect } = await import("@codeeditorland/common/Target/Function/Redirect.js");
-const { default: Response } = await import("@codeeditorland/common/Target/Function/Response.js");
-export {
-  Redirect,
-  Response,
-  Worker_default as default
-};
+var r={fetch:async(...[t,e])=>await(await import("itty-router/Router")).Router().get("*",async()=>await a({Error:"Not Found."},404)).handle(t,e)};const{default:o}=await import("@codeeditorland/common/Target/Function/Redirect.js"),{default:a}=await import("@codeeditorland/common/Target/Function/Response.js");export{o as Redirect,a as Response,r as default};
